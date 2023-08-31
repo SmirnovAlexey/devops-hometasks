984da7aa61df2408a00eefb568aecc23675aae26
Alexey Smirnov <alexey.work@tut.by>
2023-08-31 22:06:10 +0300

    Terraform example

---
+++ 13-terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "app_name" {
+    default = "wcg-app"
+}
+
+variable "app_container_name" {
+    default = "app"
+}
+
+variable "app_container_label" {
+    default = "wcg"
+}
+
+variable "app_image" {}
+
+variable "app_replicas" {}
+
+variable "service_name" {
+    default = "wcg-srv"
+}
+
+variable "service_type" {}
+
+variable "service_port" {}
+
+variable "ingress_name" {
+    default = "wcg-ingress"
+}
+
+variable "ingress_host" {}
+
+variable "ingress_path" {
+    default = "/"
+}
+
+variable "ingress_path_type" {
+    default = "Prefix"
+}
+
+variable "tf_token" {
+    default = "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"
+    sensitive = true
+    type = string
+}
+
+variable "files" {
+    default = [
+        "versions.tf",
+        "variables.tf",
+        "service.tf"
+    ]
+}
