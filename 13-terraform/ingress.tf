1881966cc12200f12ae30eaa47cc3a4d9baac171
Alexey Smirnov <alexey.work@tut.by>
2023-09-01 10:56:15 +0300

    Terraform example

---
+++ 13-terraform/ingress.tf
@@ -0,0 +1,24 @@
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    name = var.ingress_name
+  }
+  spec {
+    rule {
+      host = var.ingress_host
+      http {
+        path {
+          path      = var.ingress_path
+          path_type = var.ingress_path_type
+          backend {
+            service {
+              name = var.service_name
+              port {
+                number = var.service_port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
