7b16ec4538aeeb99df82779691052fce0f40d8cd
Alexey Smirnov <alexey.work@tut.by>
2023-09-01 10:56:12 +0300

    Terraform example

---
+++ 13-terraform/service.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_service_v1" "service" {
+  metadata {
+    name = var.service_name
+  }
+  spec {
+    selector = {
+      app = var.app_container_label
+    }
+    type = var.service_type
+    port {
+      port = var.service_port
+    }
+  }
+}
